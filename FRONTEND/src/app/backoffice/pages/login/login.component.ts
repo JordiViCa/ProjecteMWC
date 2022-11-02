b0f9dbebe29503b6bc28bd7b07ff555cb859806e
Jordi Vila <jordi.vilacast@gmail.com>
2022-11-02 18:17:34 +0100

    [FEAT] Create login structure

--- FRONTEND/src/app/backoffice/pages/login/login.component.ts
+++ FRONTEND/src/app/backoffice/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/backoffice/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  showErrors = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userSVC: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.initFormLogin()
   }
 
+  initFormLogin() {
+    this.loginForm = this.formBuilder.group({
+      username: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]],
+      remember: [false, []]
+    });
+  }
+
+  sendFormLogin() {
+    if (this.loginForm.valid) {
+      let token = this.userSVC.getToken(this.loginForm.value.username,this.loginForm.value.password)
+      if (token) { // MAKE CALL TO ENDPOINT TO CHECK CREDENTIALS
+        this.router.navigateByUrl("/backoffice")
+      } else {
+        this.showErrors = true;
+      }
+    } else {
+      this.showErrors = true;
+    }
+  }
 }
