faaf91521cbc7cf6cde5c12e90990efabacfc706
Joan Puig <joan2003024@gmail.com>
2022-12-20 13:08:12 +0100

    [UPD] dbConf

--- BACKEND/server/routes/auth.py
+++ BACKEND/server/routes/auth.py
@@ -1,5 +1,13 @@
-from flask import Blueprint, request, redirect, url_for
-from ..db import db
+from flask import Blueprint, request, redirect, url_for, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
+from ..db import Db
 
 auth = Blueprint("auth", __name__)
+
+db = Db()
+
+@auth.route('/clients', methods=['GET'])
+def clients():
+    list = [ x["nom"] for x in db.clients.find()]
+    return jsonify(list)
+    
