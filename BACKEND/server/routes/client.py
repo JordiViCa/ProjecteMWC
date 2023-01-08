565b40b8b8a49a339e44da16ba1f39a719ef4a60
Joan Puig <joan2003024@gmail.com>
2023-01-08 12:44:59 +0100

    S'ha creat la comporvació de tokens i la baixa de clients

--- BACKEND/server/routes/client.py
+++ BACKEND/server/routes/client.py
@@ -7,3 +7,13 @@ client = Blueprint("client", __name__)
 @client.route('/llistar', methods=['GET'])
 def llistar():
     return jsonify(Client.objects)
+
+@client.route('/baixa', methods=['POST'])
+def baixa():
+    email = request.args.get("email")
+    user = Client.objects(email = email).first()
+    if user is None:
+        return "L'usuari no existeix"
+    
+    user.delete()
+    return jsonify(succes=True)
