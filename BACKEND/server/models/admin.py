045ea0acb6bab83c27ff32d9cd04537ba070600a
Joan Puig <joan2003024@gmail.com>
2023-01-31 15:15:49 +0100

    [UPD] admin.email is not a primary key

--- BACKEND/server/models/admin.py
+++ BACKEND/server/models/admin.py
@@ -2,7 +2,7 @@ from .. import db
 
 
 class Admin(db.Document):
-    email = db.StringField(max_length=100, primary_key=True, required=True)
+    email = db.StringField(max_length=100, unique=True, required=True)
     name = db.StringField(max_length=100, required=True)
     password = db.StringField(required=True)
     authority = db.BooleanField(required=True)
