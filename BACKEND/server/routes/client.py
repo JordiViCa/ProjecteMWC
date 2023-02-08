451dfcf4c523333453e4f161a88ea5ec98246916
Joan Puig <joan2003024@gmail.com>
2023-02-08 13:58:34 +0100

    [FEAT] create admin crud and upd client crud with no passwords

--- BACKEND/server/routes/client.py
+++ BACKEND/server/routes/client.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, request, jsonify
 from ..models.client import Client
+from mongoengine.errors import InvalidQueryError
+
 
 clients = Blueprint("clients", __name__)
 
 
 @clients.route('/', methods=['GET'])
 def get_clients():
-    return jsonify(Client.objects)
+    clients = [client for client in Client.objects.exclude("password") ]
+    return clients
 
 @clients.route('<id>', methods=['DELETE'])
 def remove_client(id):
@@ -14,17 +17,41 @@ def remove_client(id):
     if not user:
         user = Client.objects(nif=id).first()
     if not user:
-        return "user not found", 500
+        return "user not found", 404
 
     user.delete()
     return jsonify(succes=True)
 
 @clients.route('<id>', methods=['GET'])
 def get_client(id):
+    user = Client.objects(email=id).exclude("password").first()
+    if not user:
+        user = Client.objects(nif=id).exclude("password").first()
+    if not user:
+        return "user not found", 404
+    
+    return jsonify(user)
+
+
+@clients.route('<id>', methods=['PUT'])
+def update_client(id):
+    body = request.get_json()
+    body.pop("password", None)
+    print(body)
+
     user = Client.objects(email=id).first()
     if not user:
         user = Client.objects(nif=id).first()
     if not user:
-        return "user not found", 500
-    return jsonify(user)
+        return "user not found", 404
+    try:
+        user.update(**body)
+    except InvalidQueryError:
+        return "bad request", 400
+    except Exception as e:
+        return str(e), 500
 
+    
+
+
+    return jsonify(success=True)
