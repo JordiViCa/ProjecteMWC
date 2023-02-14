20e9d95d89e4ad8f0de6414fe16a35ff52bb360e
GloriaKwofie <91749390+GloriaKwofie@users.noreply.github.com>
2023-02-14 13:36:53 +0100

    the course page

--- FRONTEND/src/app/frontoffice/pages/course/course.component.ts
+++ FRONTEND/src/app/frontoffice/pages/course/course.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent {
+  constructor() { }
+
+  ngOnIt(): void {}
+  course=[
+    {'id':1,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://images.randomhouse.com/cover/9781925418842'},
+    {'id':2,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://m.media-amazon.com/images/I/41mjFru%2B-1L._SX342_SY445_QL70_ML2_.jpg'},
+    {'id':3,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://images.randomhouse.com/cover/9781925418842'}
+
+
+  ]
 
 }
