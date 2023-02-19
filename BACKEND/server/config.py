4beb9b1be0e8632f50b0ee00a908cb16b7103c25
Joan Puig <joan2003024@gmail.com>
2023-02-19 17:54:50 +0100

    [FEAT] create and list all document

--- BACKEND/server/config.py
+++ BACKEND/server/config.py
@@ -6,6 +6,8 @@ class Config:
     DEBUG = False
     TESTING = False
     MONGODB_HOST = os.getenv("DB_URI")
+    UPLOAD_FOLDER = "server/uploads/"
+    MAX_CONTENT_LENGTH = 15*1000*1000
     
 class ProdConf(Config):
     SECRET_KEY= os.getenv("SECRET_KEY")
