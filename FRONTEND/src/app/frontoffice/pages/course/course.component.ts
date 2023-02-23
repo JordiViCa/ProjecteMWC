84af5ed3676cb9062e48656da2cd5905072ed5f4
GloriaKwofie <91749390+GloriaKwofie@users.noreply.github.com>
2023-02-23 11:48:30 +0100

    al the new changes

--- FRONTEND/src/app/frontoffice/pages/course/course.component.ts
+++ FRONTEND/src/app/frontoffice/pages/course/course.component.ts
@@ -10,9 +10,9 @@ export class CourseComponent {
 
   ngOnIt(): void {}
   course=[
-    {'id':1,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://images.randomhouse.com/cover/9781925418842'},
-    {'id':2,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://m.media-amazon.com/images/I/41mjFru%2B-1L._SX342_SY445_QL70_ML2_.jpg'},
-    {'id':3,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://images.randomhouse.com/cover/9781925418842'}
+    {'id':1,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://bebooks.io/wp-content/uploads/2021/02/libro-aprender-catalan-rapido.jpg'},
+    {'id':2,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://static.fnac-static.com/multimedia/Images/ES/NR/1d/8b/63/6523677/1507-1.jpg'},
+    {'id':3,'name':'Learn Angles','description':'this it a course about Catalan','image':'https://atelierlibrosjuridicos.com/wp-content/uploads/2022/01/9788413593180_PORTADA_EXCEDENCIA_LABORAL_PEQUE-min.jpg'}
 
 
   ]
