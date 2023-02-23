f4e5c1d6b54cee04fb108719098528e1bce75b4d
GloriaKwofie <91749390+GloriaKwofie@users.noreply.github.com>
2023-02-23 11:59:05 +0100

    the changes before the merge

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './frontoffice/pages/footer/footer.component';
+import { HeaderComponent } from './frontoffice/pages/header/header.component';
+import { LoginComponent } from './frontoffice/pages/login/login.component';
+import { SignupComponent } from './frontoffice/pages/signup/signup.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
@@ -15,6 +19,10 @@ const routes: Routes = [
     path: "**",
     component: NotFoundComponent
   },
+  {path: 'header',component:HeaderComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'signup',component:SignupComponent},
+  {path: 'footer',component:FooterComponent}
 ];
 
 @NgModule({
--- FRONTEND/src/app/frontoffice/frontoffice-routing.module.ts
+++ FRONTEND/src/app/frontoffice/frontoffice-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ArriagoComponent } from './pages/arriago/arriago.component';
+import { CourseComponent } from './pages/course/course.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 
 const routes: Routes = [
@@ -9,6 +16,14 @@ const routes: Routes = [
     path: "",
     component: LandingComponent
   },
+  {path:'arriago',component:ArriagoComponent},
+  {path:'course',component:CourseComponent},
+  {path:'footer',component:FooterComponent},
+  {path:'header',component:HeaderComponent},
+  {path:'home',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'about',component:AboutComponent},
   {
     path: ":idiom",
     children: [
